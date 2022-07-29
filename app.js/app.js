4af3b3c066fa47015393b1075960b65a9c810275
vikas <vikas70607@gmail.com>
2022-07-29 12:19:46 +0530

    hello

---
+++ app.js/app.js
@@ -0,0 +1,16 @@
+console.log("Hello world");
+
+let addbtn = document.getElementById('addBtn');
+
+addbtn.addEventListener('click',function(e){
+    let addtxt = document.getElementById('addTxt');
+    let notes = localStorage.getItem('notes');
+
+    if(notes == null){
+        notesobj = [];
+    }
+    else{
+        notesobj 
+    }
+
+})
